6e4bbd6f604b49e4c2b629f0d8074ceb19b4e951
Conor Fleming <conorfleming03@gmail.com>
2021-05-13 16:41:57 -0700

    setup of clock program

---
+++ clock/clock.go
@@ -0,0 +1,30 @@
+package clock
+
+func CreateClock() {
+	new(clock)
+
+}
+
+func AddMinutes() {
+
+}
+
+func SubtractMinutes() {
+
+}
+
+func SubtractMinutesStringless() {
+
+}
+
+func CompareClocks() {
+
+}
+
+func AddAndCompare() {
+
+}
+
+func SubtractAndCompare() {
+
+}
