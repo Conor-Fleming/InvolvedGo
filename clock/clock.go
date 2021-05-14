3c62c5f589df5ebbbef601e8e285431b2b616f0a
Conor Fleming <conorfleming03@gmail.com>
2021-05-13 21:51:10 -0700

    clock work

--- clock/clock.go
+++ clock/clock.go
@@ -1,7 +1,25 @@
 package clock
 
-func CreateClock() {
-	new(clock)
+import "fmt"
+
+type Clock 
+New(hour, minute int) Clock
+
+func CreateClock(hour int, minute int) string {
+	New()
+	hour = hour % 24
+	tempMin := minute
+	minute = minute % 60
+
+	//use tempMin (initial number of minutes to update hour)
+	if (tempMin / 60) > 24 {
+		hour += tempMin/60 - 24
+	} else {
+		hour += tempMin / 60
+	}
+
+	fmt.Println(hour)
+	fmt.Println(minute)
 
 }
 
