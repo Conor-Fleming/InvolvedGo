e6019245d6d2e8948c3955bd618d3c02508be2fb
Conor Fleming <conorfleming03@gmail.com>
2021-05-15 11:21:16 -0700

    clock object and funcitons

--- clock/clock.go
+++ clock/clock.go
@@ -1,3 +1,4 @@
+//Clock package gives us functions to manipulate clocks and time
 package clock
 
 import "fmt"
@@ -11,13 +12,18 @@ func (clock Clock) String() string {
 	return fmt.Sprintf("%.2v:%.2v", clock.Hours, clock.Minutes)
 }
 
+//New creates a new clock with two input values for hour and minute
 func New(hour int, minute int) Clock {
 	//manipulting values to ensure correct hour and minute values
-	hour = (hour % 24) + ((minute / 60) % 24)
+	hour = (hour + (minute / 60)) % 24
 	minute = minute % 60
 
+	if minute < 0 {
+		hour--
+		minute = 60 + minute
+	}
 	if hour < 0 {
-
+		hour = 24 + hour
 	}
 
 	myClock := Clock{
@@ -27,11 +33,13 @@ func New(hour int, minute int) Clock {
 	return myClock
 }
 
+//Add lets you add values to a clock
 func (clock Clock) Add(minutes int) Clock {
 	return New(clock.Hours, clock.Minutes+minutes)
 
 }
 
+//Subtract lets you take minutes from an existing clock
 func (clock Clock) Subtract(minutes int) Clock {
 	return New(clock.Hours, clock.Minutes-minutes)
 }
