c683a9e49c271bbcb8f091e9b3cf69a32eaf043b
Conor Fleming <conorfleming03@gmail.com>
2021-05-19 15:57:02 -0700

    passing tests

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -1,5 +1,10 @@
 package tree
 
+import (
+	"fmt"
+	"sort"
+)
+
 type Record struct {
 	ID     int
 	Parent int
@@ -10,16 +15,30 @@ type Node struct {
 }
 
 func Build(records []Record) (*Node, error) {
-	tree := make([]*Node, 0)
-	root := Node
-	for records != nil {
-		for i, v := range records {
-			if v.ID == 0 {
-				root := Node{0, nil}
-				tree[i] = &root
+	fmt.Println(records)
+	if len(records) == 0 {
+		return nil, nil
+	}
+	tree := make([]*Node, len(records))
 
-			}
+	sort.SliceStable(records, func(i, j int) bool { return records[i].ID < records[j].ID })
+	fmt.Println(records)
+	for i, v := range records {
+		//checking for valid input of records
+		if v.ID != i {
+			return nil, fmt.Errorf("invalid input, cannot create tree:%+v ", v)
+		}
+		if v.ID < v.Parent {
+			return nil, fmt.Errorf("invalid input, cannot create tree:%+v ", v)
+		}
+		if i != 0 && v.Parent == i {
+			return nil, fmt.Errorf("invalid input, cannot create tree:%+v ", v)
+		}
+
+		tree[v.ID] = &Node{ID: v.ID}
+		if v.ID != 0 {
+			tree[v.Parent].Children = append(tree[v.Parent].Children, tree[v.ID])
 		}
 	}
-
+	return tree[0], nil
 }
