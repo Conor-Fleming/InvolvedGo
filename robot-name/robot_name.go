a7199d036aeca01f05d9963e1ba7603e4bebcc92
Conor Fleming <conorfleming03@gmail.com>
2021-05-21 15:18:46 -0700

    generating random letters for robot name

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -1,5 +1,39 @@
 package robotname
 
-func Name() {
+import (
+	"math/rand"
+	"time"
+)
 
+const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+type Robot struct {
+	name string
+}
+
+//namesUsed := make(map[string]int)
+
+func (r *Robot) Name() (string, error) {
+	//create letters/numbers for name and check if name is in use in map namesUsed
+
+	name := createName()
+	return name, nil
+}
+
+func (r *Robot) Reset() {
+
+}
+func createLetters() string {
+	letters := make([]byte, 2)
+	for i := range letters {
+		letters[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(letters)
+}
+func createNumbers
+
+func robotNameCheck() bool {
+	return true
 }
