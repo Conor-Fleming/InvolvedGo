9223f5f2240f519d596fe1d4496de4723b14c94e
Conor Fleming <conorfleming03@gmail.com>
2021-05-24 07:38:04 -0700

    cleanup

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -6,11 +6,6 @@ import (
 	"math/rand"
 )
 
-/*const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numbset = "1234567890"
-
-var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))*/
-
 //Robot type creates a robot object with a property of name
 type Robot struct {
 	name string
@@ -42,25 +37,3 @@ func newName() string {
 	num := rand.Intn(1000)
 	return fmt.Sprintf("%c%c%03d", r1, r2, num)
 }
-
-/*func createLetters() string {
-	letters := make([]byte, 2)
-	for i := range letters {
-		letters[i] = charset[seededRand.Intn(len(charset))]
-	}
-	return string(letters)
-}
-func createNumbers() string {
-	numbers := make([]byte, 3)
-	for i := range numbers {
-		numbers[i] = numbset[seededRand.Intn(len(numbset))]
-	}
-	return string(numbers)
-}
-
-func robotNameCheck(name string) bool {
-	if _, present := namesUsed[name]; present {
-		return true
-	}
-	return false
-}*/
