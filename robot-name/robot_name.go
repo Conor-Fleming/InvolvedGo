1b496d0aff149066828f70040d7503945a844b2d
Conor Fleming <conorfleming03@gmail.com>
2021-05-24 08:58:35 -0700

    optimizing, realized some bits were not needed so took them out ie. reset function return. also implemented early retunr in Name().

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -4,7 +4,10 @@ package robotname
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
+
+var seededRand *rand.Rand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 //Robot type creates a robot object with a property of name
 type Robot struct {
@@ -15,25 +18,26 @@ var namesUsed = map[string]bool{}
 
 //Name function checks to see if a Robot has a name
 func (r *Robot) Name() (string, error) {
-	//create letters/numbers for name and check if name is in use in map namesUsed
-	if r.name == "" {
+	//create name and check if name is in use in map namesUsed
+	if r.name != "" {
+		return r.name, nil
+	}
+	r.name = newName()
+	for namesUsed[r.name] {
 		r.name = newName()
-		for namesUsed[r.name] {
-			r.name = newName()
-		}
 	}
 	return r.name, nil
 }
 
-//Reset function helps with the clearing of the robots name and assignment of a new name with a call to Name()
-func (r *Robot) Reset() (string, error) {
+//Reset function helps with the clearing of the robots name
+func (r *Robot) Reset() {
 	r.name = ""
-	return r.Name()
 }
 
+//NewName helps with generation of robot names
 func newName() string {
-	r1 := rand.Intn(26) + 'A'
-	r2 := rand.Intn(26) + 'A'
-	num := rand.Intn(1000)
+	r1 := seededRand.Intn(26) + 'A'
+	r2 := seededRand.Intn(26) + 'A'
+	num := seededRand.Intn(1000)
 	return fmt.Sprintf("%c%c%03d", r1, r2, num)
 }
