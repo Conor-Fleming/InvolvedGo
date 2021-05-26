932fb93c8a3ecac332e592fe228d19637c4c02c2
Conor Fleming <conorfleming03@gmail.com>
2021-05-26 15:24:16 -0700

    acheived output to console

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -1,10 +1,13 @@
 package tournament
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"io"
 )
 
-type Team struct {
+type TeamRecord struct {
 	name   string
 	wins   int
 	losses int
@@ -13,21 +16,22 @@ type Team struct {
 	points int
 }
 
-func newTeam(name string) *Team {
-	t := Team{name: name}
-	t.wins = 0
-	t.losses = 0
-	t.draws = 0
-	t.played = 0
-	t.points = 0
-	return &t
+//Results map holds team names
+var results = map[string]TeamRecord{}
+
+func Tally(reader io.Reader, writer io.Writer) error {
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		//fmt.Println(scanner.Text())
+	}
+
+	writeBoard(results)
+	return nil
 }
 
-func Tally(input io.Reader, output io.Writer) error {
-	AA := newTeam("Allegoric Alaskins")
-	BB := newTeam("Blithering Badgers")
-	CC := newTeam("Courageous Californians")
-	DD := newTeam("Devastating Donkeys")
-
-	return nil
+func writeBoard(results map[string]TeamRecord) {
+	writer := bytes.NewBufferString("")
+	testStr := "testing writing"
+	fmt.Fprint(writer, " some space in between ", testStr)
+	fmt.Println(writer.String())
 }
