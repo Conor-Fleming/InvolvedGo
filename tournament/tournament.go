a3688cfcf194093686203c464f8b9dfd704e8c2d
Conor Fleming <conorfleming03@gmail.com>
2021-05-25 17:13:40 -0700

    creating team structure and init function for new teams

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -1,8 +1,32 @@
 package tournament
 
-import "io"
+import (
+	"fmt"
+	"io"
+)
 
-func Tally(io.Reader, io.Writer) error {
+type Team struct {
+	name   string
+	wins   int
+	losses int
+	draws  int
+	played int
+	points int
+}
+
+func newTeam(name string) *Team {
+	t := Team{name: name}
+	t.wins = 0
+	t.losses = 0
+	t.draws = 0
+	t.played = 0
+	t.points = 0
+	return &t
+}
+
+func Tally(input io.Reader, output io.Writer) error {
+	AA := newTeam("AA")
+	fmt.Println(AA)
 
 	return nil
 }
