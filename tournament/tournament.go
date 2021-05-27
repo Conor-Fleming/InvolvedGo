16b50aa71ae172b21cc8249c74da25878ec10c6d
Conor Fleming <conorfleming03@gmail.com>
2021-05-26 17:21:58 -0700

    added switch and working on board printing

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"strings"
 )
 
 type TeamRecord struct {
@@ -16,13 +17,43 @@ type TeamRecord struct {
 	points int
 }
 
-//Results map holds team names
-var results = map[string]TeamRecord{}
-
 func Tally(reader io.Reader, writer io.Writer) error {
+	//by creating your map with make you then have access to the fields inside TeamRecord
+	results := make(map[string]TeamRecord)
 	scanner := bufio.NewScanner(reader)
 	for scanner.Scan() {
+		if scanner.Text() == "" || strings.HasPrefix(scanner.Text(), "#") {
+			continue
+		}
 		//fmt.Println(scanner.Text())
+		split := strings.Split(scanner.Text(), ";")
+
+		team1 := results[split[0]]
+		//team1.name = split[0]
+		team2 := results[split[1]]
+		//team2.name = split[1]
+		//switch statement handling the tallying of points and games based off results of matches
+		switch split[2] {
+		case "win":
+			team1.wins++
+			team1.played++
+			team2.played++
+			team2.losses++
+			team1.points += 3
+		case "loss":
+			team1.losses++
+			team1.played++
+			team2.played++
+			team2.wins++
+			team2.points += 3
+		case "draw":
+			team1.played++
+			team2.played++
+			team1.points++
+			team2.points++
+		default:
+			return fmt.Errorf("input formatted incorrectly")
+		}
 	}
 
 	writeBoard(results)
@@ -30,8 +61,7 @@ func Tally(reader io.Reader, writer io.Writer) error {
 }
 
 func writeBoard(results map[string]TeamRecord) {
-	writer := bytes.NewBufferString("")
-	testStr := "testing writing"
-	fmt.Fprint(writer, " some space in between ", testStr)
+	header := fmt.Sprintf("Team\t\t\t\t| MP | W | D | L | P\n")
+	writer := bytes.NewBufferString(header)
 	fmt.Println(writer.String())
 }
