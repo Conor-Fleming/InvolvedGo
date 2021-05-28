706cd6dec39a3ef137ddf392e7324cff95ec08c4
Conor Fleming <conorfleming03@gmail.com>
2021-05-28 07:35:41 -0700

    working on updating each struct accordingly within the map

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -28,10 +28,12 @@ func Tally(reader io.Reader, writer io.Writer) error {
 		//fmt.Println(scanner.Text())
 		split := strings.Split(scanner.Text(), ";")
 
-		team1 := results[split[0]]
-		//team1.name = split[0]
-		team2 := results[split[1]]
-		//team2.name = split[1]
+		if team1, ok := results[split[0]]; !ok {
+			team1.name = split[0]
+		}
+		if team2, ok := results[split[1]]; !ok {
+			team2.name = split[1]
+		}
 		//switch statement handling the tallying of points and games based off results of matches
 		switch split[2] {
 		case "win":
@@ -40,20 +42,26 @@ func Tally(reader io.Reader, writer io.Writer) error {
 			team2.played++
 			team2.losses++
 			team1.points += 3
+			break
 		case "loss":
 			team1.losses++
 			team1.played++
 			team2.played++
 			team2.wins++
 			team2.points += 3
+			break
 		case "draw":
 			team1.played++
 			team2.played++
 			team1.points++
 			team2.points++
+			break
 		default:
 			return fmt.Errorf("input formatted incorrectly")
 		}
+	}
+	for i := range results {
+		results[i].name = results[i]
 	}
 
 	writeBoard(results)
@@ -62,6 +70,11 @@ func Tally(reader io.Reader, writer io.Writer) error {
 
 func writeBoard(results map[string]TeamRecord) {
 	header := fmt.Sprintf("Team\t\t\t\t| MP | W | D | L | P\n")
+	stats := ""
+	for i := range results {
+		stats += fmt.Sprintf("Team\t\t\t\t| %v | %v | %v | %v | %v\n", results[i].played, results[i].wins, results[i].draws, results[i].losses, results[i].points)
+	}
+	header += stats
 	writer := bytes.NewBufferString(header)
-	fmt.Println(writer.String())
+	writer.String()
 }
