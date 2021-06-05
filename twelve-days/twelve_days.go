a1b400325b399955183221524eb10a487dca766e
Conor Fleming <conorfleming03@gmail.com>
2021-06-05 12:35:28 -0700

    verses printing using switch statement

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -1,1 +1,50 @@
 package twelve
+
+import "fmt"
+
+func Song() string {
+	return "testing"
+}
+
+func Verse(verse int) string {
+	output := ""
+	switch verse {
+	case 1:
+		output = fmt.Sprintf("On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.")
+		break
+	case 2:
+		output = fmt.Sprintf("On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.")
+		break
+	case 3:
+		output = fmt.Sprintf("On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+		break
+	case 4:
+		output = fmt.Sprintf("On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+		break
+	case 5:
+		output = fmt.Sprintf("On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+		break
+	case 6:
+		output = fmt.Sprintf("On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+		break
+	case 7:
+		output = fmt.Sprintf("On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+		break
+	case 8:
+		output = fmt.Sprintf("On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+		break
+	case 9:
+		output = fmt.Sprintf("On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+		break
+	case 10:
+		output = fmt.Sprintf("On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+		break
+	case 11:
+		output = fmt.Sprintf("On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+		break
+	case 12:
+		output = fmt.Sprintf("On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.")
+		break
+	}
+	return output
+}
