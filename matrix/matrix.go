8f190370e692187abc6b325fdd8c7a93b114be81
Conor Fleming <conorfleming03@gmail.com>
2021-06-28 15:59:13 -0700

    skeleton file

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,1 +1,19 @@
 package matrix
+
+type Matrix [][]int
+
+func (m *Matrix) Rows() [][]int {
+
+}
+
+func (m *Matrix) Cols() [][]int {
+
+}
+
+func (m *Matrix) Set(row, col, val) {
+
+}
+
+func New() (Matrix, error) {
+
+}
